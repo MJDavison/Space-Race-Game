8382b43e4d4157029dbf9cd913c506b12e1116df
MJDeveloping <63269417+MJDeveloping@users.noreply.github.com>
2020-12-24 00:33:43 +0000

    Implemented timer.
    I played around a bit trying to get a rect transform to shrink from the top- and I succeeded! however I realised that I couldn't figure out any method of adjusting it so that it would be complete in exactly 45 seconds (the minimum game time). Instead I went the old classic route: manipulating the slider.

---
+++ Space-Race-Game/Assets/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    [Range(45,180)][SerializeField] public float fullGameTime;
+    public float gameTimeRemaining;
+    [SerializeField] float elapsedGameTime;
+    [SerializeField] public bool gameActive = false;
+    
+    [Range(1,2)][SerializeField] public float gameSpeed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameLoop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameLoop();
+    }
+
+    private void GameLoop(){
+        if(gameActive){
+            elapsedGameTime += Time.deltaTime;
+            gameTimeRemaining = fullGameTime - elapsedGameTime;
+
+            if(gameTimeRemaining == 0){
+                gameActive = false;
+            }
+        }
+    }
+
+    public void GameStart(){
+        gameActive = true;
+    }
+
+
+}
---
+++ Space-Race-Game/Assets/GameUIManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUIManager : MonoBehaviour
+{
+    [SerializeField] GameManager myGM;
+    [SerializeField] GameObject timerHolder;
+    [SerializeField] Slider mySlider;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //mySlider = timerHolder.GetComponent<Slider>();
+        mySlider.maxValue = myGM.fullGameTime;
+        mySlider.minValue = 0;        
+    }    
+    // Update is called once per frame
+      
+    void Update()
+    {               
+        // print(timerImage.GetComponent<RectTransform>().anchorMax);
+        // if(timerImage.GetComponent<RectTransform>().anchorMax.y < 0f){
+        //     timerImage.GetComponent<RectTransform>().anchorMax = new Vector2(timerImage.GetComponent<RectTransform>().anchorMax.x,timerImage.GetComponent<RectTransform>().anchorMax.y-(Time.deltaTime/myGM.gameSpeed));
+        // }
+
+        mySlider.value = myGM.gameTimeRemaining;
+
+        
+        
+        
+        
+
+    }
+}
