9a0a5e1df936ebb95144fbf5fc396891e62cb097
MJDeveloping <63269417+MJDeveloping@users.noreply.github.com>
2020-12-24 12:47:23 +0000

    Made some small changes
    Simply put, I swapped from the old input system to the new one by directly reading device input. I had issues with doing split keyboard controls using the indirect (but recommended) method of the "Player Input" component.

--- Space-Race-Game/Assets/PlayerController.cs
+++ Space-Race-Game/Assets/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 
 public class PlayerController : MonoBehaviour
@@ -13,47 +14,73 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         myRB = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(playerID == 1){
-            if(Input.GetKey(KeyCode.W)){
-                movementInput = 1;
-                print("W Pressed");
-            }
+        // if(playerID == 1){
+        //     if(Input.GetKey(KeyCode.W)){
+        //         movementInput = 1;
+        //         print("W Pressed");
+        //     }
                             
-            if(Input.GetKey(KeyCode.S)){
-                movementInput = -1;
-                print("S Pressed");
-            } 
+        //     if(Input.GetKey(KeyCode.S)){
+        //         movementInput = -1;
+        //         print("S Pressed");
+        //     } 
 
-            if(Input.GetKeyUp(KeyCode.W)||Input.GetKeyUp(KeyCode.S)){
+        //     if(Input.GetKeyUp(KeyCode.W)||Input.GetKeyUp(KeyCode.S)){
+        //         movementInput = 0;
+        //         print("W or S Released");
+        //     }           
+        // }
+
+        // if(playerID == 2){
+        //     if(Input.GetKey(KeyCode.UpArrow)){
+        //         movementInput = 1;
+        //         print("Up Arrow Pressed");
+        //     }
+                            
+        //     if(Input.GetKey(KeyCode.DownArrow)){
+        //         movementInput = -1;
+        //          print("Down Arrow Pressed");
+        //     } 
+
+        //     if(Input.GetKeyUp(KeyCode.UpArrow)||Input.GetKeyUp(KeyCode.DownArrow)){
+        //         movementInput = 0;
+        //         print("Up or Down Arrow Released");
+        //     }           
+        // }
+
+        if(playerID == 1){
+            if(Keyboard.current.wKey.isPressed){
+                movementInput = 1;
+                // print("W Pressed");
+            } else if(Keyboard.current.sKey.isPressed){
+                movementInput = -1;
+                // print("S Pressed");
+            } 
+            else{
                 movementInput = 0;
-                print("W or S Released");
-            }           
+            }                                                                
         }
 
         if(playerID == 2){
-            if(Input.GetKey(KeyCode.UpArrow)){
+            if(Keyboard.current.upArrowKey.isPressed){
                 movementInput = 1;
-                print("Up Arrow Pressed");
-            }
-                            
-            if(Input.GetKey(KeyCode.DownArrow)){
+                // print("W Pressed");
+            } else if(Keyboard.current.downArrowKey.isPressed){
                 movementInput = -1;
-                 print("Down Arrow Pressed");
-            } 
-
-            if(Input.GetKeyUp(KeyCode.UpArrow)||Input.GetKeyUp(KeyCode.DownArrow)){
+                // print("S Pressed");
+            } else{
                 movementInput = 0;
-                print("Up or Down Arrow Released");
-            }           
+            }                                                        
         }
 
         
-        print(movementInput);
+        // print(movementInput);
 
         ApplyMovement(movementInput);
     }
