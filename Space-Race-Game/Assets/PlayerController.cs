2a54e22efd6fddd6a07c05790cedba08b47f715c
MJDeveloping <63269417+MJDeveloping@users.noreply.github.com>
2020-12-24 01:48:26 +0000

    Added Player Movement
    Player 1 can move with WASD and Player 2 can move with Arrow keys. Nothing blocking them going off screen yet though.

---
+++ Space-Race-Game/Assets/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D myRB;
+    [SerializeField] internal float movementSpeed = 10f;
+    internal float movementInput;
+    [SerializeField] internal int playerID;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerID == 1){
+            if(Input.GetKey(KeyCode.W)){
+                movementInput = 1;
+                print("W Pressed");
+            }
+                            
+            if(Input.GetKey(KeyCode.S)){
+                movementInput = -1;
+                print("S Pressed");
+            } 
+
+            if(Input.GetKeyUp(KeyCode.W)||Input.GetKeyUp(KeyCode.S)){
+                movementInput = 0;
+                print("W or S Released");
+            }           
+        }
+
+        if(playerID == 2){
+            if(Input.GetKey(KeyCode.UpArrow)){
+                movementInput = 1;
+                print("Up Arrow Pressed");
+            }
+                            
+            if(Input.GetKey(KeyCode.DownArrow)){
+                movementInput = -1;
+                 print("Down Arrow Pressed");
+            } 
+
+            if(Input.GetKeyUp(KeyCode.UpArrow)||Input.GetKeyUp(KeyCode.DownArrow)){
+                movementInput = 0;
+                print("Up or Down Arrow Released");
+            }           
+        }
+
+        
+        print(movementInput);
+
+        ApplyMovement(movementInput);
+    }
+    void ApplyMovement(float movementInput){
+        myRB.velocity = Vector2.up * movementInput * movementSpeed;
+    }
+}
